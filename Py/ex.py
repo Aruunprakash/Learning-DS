881e200141442aa10544b1148c9d9a6ae60cce67
Aruunprakash <aruunmp@gmail.com>
2026-04-13 14:07:16 +0530

    example of if statement

--- Py/ex.py
+++ Py/ex.py
@@ -5,10 +5,24 @@ area=0.5*(base*height)
 print(area)
 
 #if statement
-n=int(input("enter the number:")
+# 1
+n=int(input("enter the number:"))
 if n%2==0:
       print("Even")
 else:
       print("Odd")
-      
+
+indian=["vada","idli","dosa"]
+chinese=["noodles","fried rice"]
+italian=["pizza","pasta"]
+
+dish=input("enter your favorite dish:")
+if dish in indian:
+    print("your favorite dish is indian")
+elif dish in italian:
+    print("your favorite dish is italian")
+elif dish in chinese:
+    print("your favorite dish is chinese")
+else:
+    print("your favorite dish is not on the list")
 
