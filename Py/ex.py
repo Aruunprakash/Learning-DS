33d29e640253aa104de1504116f61632966d504f
Arun <155754381+Aruunprakash@users.noreply.github.com>
2026-04-13 13:26:09 +0530

    if statement ex

--- Py/ex.py
+++ Py/ex.py
@@ -1,7 +1,14 @@
+#Area of triangle
 base=10
 height=5
 area=0.5*(base*height)
 print(area)
 
-
+#if statement
+n=int(input("enter the number:")
+if n%2==0:
+      print("Even")
+else:
+      print("Odd")
+      
 
